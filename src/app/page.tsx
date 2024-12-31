b43033cc9e6f138bb62a73b2fc1a5263f2c0b063
SamFaMH <121002154+SamFaMH@users.noreply.github.com>
2024-12-31 10:47:21 +0530

    Changed: page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-import { CardWithBar } from "@/components/Dashboard/SkillsRequested";
-import { CardWithChart } from "@/components/Dashboard/TimeSpentChart";
+// page.tsx
+'use client';
+import ChatInput from "@/components/messageSystem/chatInput";
+import Navbar from "../components/Navbar";
 
-const Home = () => {
+
+export default function Home() {
   return (
-   
-
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold text-center mb-6">Dashboard</h1>
-      <CardWithChart />
-      <div>
-      <CardWithBar/>
-      </div>
-    </div>
-    
-    
-  )
+      <>
+        <div className="p-8">
+         <Navbar userName={"Nethmal"} userImage={""} />
+        </div>
+        
+      </>
+  );
 }
 
-export default Home;
+
+     
