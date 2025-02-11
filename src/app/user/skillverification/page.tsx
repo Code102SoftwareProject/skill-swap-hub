cf45dbe5154bb3acde7129b75353e91e7c305fb9
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-02-12 02:27:37 +0530

    add skill verification feature with API endpoints and model

---
+++ src/app/user/skillverification/page.tsx
@@ -0,0 +1,8 @@
+import SkillVerificationPortal from '@/components/User/SkillVerificationPortal';
+
+export default function Home() {
+  
+  const userId = 'user123';
+  return <SkillVerificationPortal userId={userId} />;
+
+}
