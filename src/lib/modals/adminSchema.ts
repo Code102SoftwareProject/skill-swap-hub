5072d92365f45a85b6065b8aef3b31614af10847
RENULUCSHMI PRAKASAN <renulucshmi@gmail.com>
2025-02-15 21:58:17 +0530

    Admin schema

--- src/lib/modals/adminSchema.ts
+++ src/lib/modals/adminSchema.ts
@@ -1,15 +1,13 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import mongoose, {Schema,model,models} from 'mongoose';
 
-const adminSchema = new Schema(
-  {
-    adminId: { type: String, required: true, unique: true },
-    name: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
+const adminSchema = new Schema({
+  adminId:{type:String,required:true,unique:true},
+    name:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true,unique:true},
+},
+    { timestamps: true }
 );
+const AdminSchema=models.AdminSchema||model("admin",adminSchema);
 
-const Admin = models.Admin || model("Admin", adminSchema);
-
-export default Admin;
+export default { adminSchema };
