cc14a46d21e5d45e4aeacc16264fcb3c23148bd1
RENULUCSHMI PRAKASAN <renulucshmi@gmail.com>
2025-02-16 19:26:25 +0530

    Error

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -1,4 +1,13 @@
-import {NextResponse}    from "next/server";
-export const GET=()=>{
-    return new NextResponse("This is myFirst API");
-};
+import connect from "@/lib/db";
+import {NextResponse} from "next/server";
+import Admin from "@/lib/modals/adminSchema"
+
+export const GET=async(req: Request, res: Response) => {
+    try{
+        await connect();
+        const admins=await Admin.find();
+        return NextResponse.json(admins, { status: 200 });
+    }catch(error:any){
+        return new NextResponse("Error in fetching admin" + error.message,{status:500});
+
+    }};
