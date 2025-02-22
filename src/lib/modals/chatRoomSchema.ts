8f419be32fe4eb43a58c0662d69ebb0b0ed3bf0a
AdeepaK2 <adeepashashintha@gmail.com>
2025-02-22 10:27:55 +0530

    online offline issue resoved complet

--- src/lib/modals/chatRoomSchema.ts
+++ src/lib/modals/chatRoomSchema.ts
@@ -11,9 +11,11 @@ export interface IChatRoom extends Document {
 }
 
 const ChatRoomSchema: Schema<IChatRoom> = new Schema<IChatRoom>({
-  participants: [
-    { type: String, ref: 'User', required: true }, // store user IDs as strings
-  ],
+  participants: [{
+    type: String,
+    ref: 'users',
+    required: true,
+  }],
   createdAt: { type: Date, default: Date.now },
   lastMessage: {
     content: { type: String },
@@ -22,9 +24,18 @@ const ChatRoomSchema: Schema<IChatRoom> = new Schema<IChatRoom>({
   },
 });
 
+// Add validation for exactly 2 participants
+ChatRoomSchema.pre('save', function(next) {
+  if (this.participants.length !== 2) {
+    next(new Error());
+  }else{
+    next();
+  }
+});
+
 // Ensure unique chat rooms for the same pair of participants
 ChatRoomSchema.index(
-  { participants: 1, participants: -1 }, 
+  { participants: 1 }, 
   { unique: true }
 );
 
