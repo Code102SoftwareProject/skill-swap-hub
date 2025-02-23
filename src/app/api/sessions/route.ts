0ad0e26039fa13e7c7bcef69ea8f0f3d220e6a53
AdeepaK2 <adeepashashintha@gmail.com>
2025-02-23 19:46:19 +0530

    Implement POST route for session creation and update session schema to include necessary fields

--- src/app/api/sessions/route.ts
+++ src/app/api/sessions/route.ts
@@ -1,3 +1,24 @@
 import { NextResponse } from "next/server";
 import connect from "@/lib/db";
 import Session from "@/lib/models/sessionSchema";
+
+export async function POST(req:Request){
+    await connect();
+    const body = await req.json();
+    const { user1Id, skill1Id, descriptionOfService1, descriptionOfService2, user2Id, skill2Id, startDate, dueDate, isAccepted, isAmmended, status } = body;
+    const session = new Session({
+        user1Id,
+        skill1Id,
+        descriptionOfService1,
+        descriptionOfService2,
+        user2Id,
+        skill2Id,
+        startDate,
+        dueDate,
+        isAccepted,
+        isAmmended,
+        status
+    });
+    await session.save();
+    return NextResponse.json({ session },{status:201});
+}
