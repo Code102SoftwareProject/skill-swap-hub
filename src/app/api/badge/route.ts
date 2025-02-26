a97ba0448fc15012ecadce971bf8aadec08036f4
RENULUCSHMI PRAKASAN <renulucshmi@gmail.com>
2025-02-26 14:37:53 +0530

    Badge endpoint  API Patch

--- src/app/api/badge/route.ts
+++ src/app/api/badge/route.ts
@@ -31,4 +31,33 @@ export const POST = async (req: NextRequest) => {
     }
 };
 
+export const PATCH = async (req: NextRequest) => {
+    try {
+        const body = await req.json();
+        const { badgeId,badgeImage, description } = body;
+
+        if (!badgeId ) {
+            return NextResponse.json({ message: "BadgeId is not found" }, { status: 400 });
+        }
+        if(!badgeImage){
+            return NextResponse.json({ message: "Badge Image is not found" }, { status: 400 });
+        }
+        
+        if(!description){
+            return NextResponse.json({ message: "Description is not found" }, { status: 400 });
+        }
+if(!Types.ObjectId.isValid(badgeId)) {
+            return NextResponse.json({ message: "Invalid badgeId" }, { status: 400 });
+        }
+
+        await connect();
+        const updatedAdmin = await Badge.findByIdAndUpdate(badgeId, { badgeImage: badgeImage, description: description }, { new: true });
+
+        return NextResponse.json({ message: "Badge updated successfully", Admin: updatedAdmin }, { status: 200 });
+        } catch (error: any) {
+            return NextResponse.json({ message: "Error in updating Badge", error: error.message }, { status: 500 });
+        }
+
+    };
+
 
