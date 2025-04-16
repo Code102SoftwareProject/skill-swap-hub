a839e395121b35950b3cfb8f073d0e0f74cf19b1
AdeepaK2 <adeepashashintha@gmail.com>
2025-04-16 10:48:21 +0530

    refactor: update tailwind configuration to use ES module syntax

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,27 @@
-/** @type {import('tailwindcss').Config} */ 
-module.exports = {
-	darkMode: "class",
-	content: [
-	  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	  "./src/app/*//.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-	  extend: {
-		colors: {
-		  primary: "var(--primary)",
-		  secondary: "var(--secondary)",
-		  grayfill: "var(--grayfill)",
-		  textcolor: "var(--textcolor)",
-		  bgcolor:"var(--bgcolor)" // Added border color definition
-		},
-		fontFamily: {
-		  heading: ["Manrope", "sans-serif"],
-		  body: ["Roboto", "sans-serif"],
-		},
-	  },
-	},
-	plugins: [require("tailwindcss-animate")],
-  };
-  
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: "class",
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/*//.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        grayfill: "var(--grayfill)",
+        textcolor: "var(--textcolor)",
+        bgcolor: "var(--bgcolor)"
+      },
+      fontFamily: {
+        heading: ["Manrope", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
